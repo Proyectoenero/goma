74d13cc7dab780b86af1096b6295718a07c783c3
Proyectoenero <123704505+Proyectoenero@users.noreply.github.com>
2023-02-01 22:22:47 -0300

    Add files via upload

---
+++ prueba/anima.js
@@ -0,0 +1,20 @@
+
+
+
+document.getElementById("boton").onclick = function() {
+    
+
+    console.log("click");
+    document.getElementById("demo").innerHTML = "Avtivebr 2810-1540"
+}
+;
+
+
+
+
+
+
+
+
+
+
